569e61c6ab40e0522f42aac6d2c276edb2f3d849
John Davidson <higheranimals@gmail.com>
2012-08-25 14:36:28 -0700

    Finishes initial db generation script.

--- scripts/make_sqlite_db.py
+++ scripts/make_sqlite_db.py
@@ -3,6 +3,7 @@
 import json
 import sqlite3
 import os
+import sys
 
 # Enums
 class Noun:
@@ -26,53 +27,86 @@ class Join:
   INCORRECT = "incorrect"
 
 # Create DB
-FILENAME = 'measure_words.sqlite3'
+FILENAME = 'assets/initial_db.sqlite3'
 
 if os.path.exists(FILENAME):
   os.unlink(FILENAME)
-cur = sqlite3.connect(FILENAME).cursor()
+con = sqlite3.connect(FILENAME)
+cur = con.cursor()
 
-data = 'CREATE TABLE "android_metadata" ("locale" TEXT DEFAULT "en_US")'
+data = 'CREATE TABLE "android_metadata" ("locale" TEXT DEFAULT "en_US");'
 print data
 cur.execute(data)
 
-data = 'INSERT INTO "android_metadata" VALUES ("en_US")'
+data = 'INSERT INTO "android_metadata" VALUES ("en_US");'
 print data
 cur.execute(data)
 
-data = ('CREATE TABLE "' + Noun.TABLE + '" (' +
-  '"' + Noun.ID + '" INT PRIMARY KEY, ' +
-  '"' + Noun.HANZI + '" TEXT, ' +
-  '"' + Noun.PINYIN + '" TEXT, ' +
-  '"' + Noun.ENGLISH + '" TEXT)')
-print data
-cur.execute(data)
+qry = ('CREATE TABLE "%s" ("%s" INTEGER PRIMARY KEY AUTOINCREMENT, "%s" TEXT, "%s" TEXT, "%s" TEXT);' % (
+  Noun.TABLE, Noun.ID, Noun.HANZI, Noun.PINYIN, Noun.ENGLISH
+))
+print qry
+cur.execute(qry)
 
-data = ('CREATE TABLE "' + MeasureWord.TABLE + '" (' +
-  '"' + MeasureWord.ID + '" INT PRIMARY KEY, ' +
-  '"' + MeasureWord.HANZI + '" TEXT, ' +
-  '"' + MeasureWord.PINYIN + '" TEXT, ' +
-  '"' + MeasureWord.ENGLISH + '" TEXT)')
-print data
-cur.execute(data)
+qry = ('CREATE TABLE "%s" ("%s" INTEGER PRIMARY KEY AUTOINCREMENT, "%s" TEXT, "%s" TEXT, "%s" TEXT);' % (
+  MeasureWord.TABLE, MeasureWord.ID, MeasureWord.HANZI,
+  MeasureWord.PINYIN, MeasureWord.ENGLISH
+))
+print qry
+cur.execute(qry)
 
-data = ('CREATE TABLE "' + Join.TABLE + '" (' +
-  '"' + Join.ID + '" INT PRIMARY KEY, ' +
-  '"' + Join.NOUN_ID + '" INT, ' +
-  '"' + Join.MEASURE_WORD_ID + '" INT, ' +
-  '"' + Join.CORRECT + '" INT DEFAULT 0, '
-  '"' + Join.INCORRECT + '" INT DEFAULT 0, ' +
-  'FOREIGN KEY ("' + Join.NOUN_ID + '") REFERENCES "' +
-      Noun.TABLE + '" ("' + Noun.ID + '"), ' +
-  'FOREIGN KEY ("' + Join.MEASURE_WORD_ID + '") REFERENCES "' +
-      MeasureWord.TABLE + '" ("' + MeasureWord.ID + '"))')
-print data
-cur.execute(data)
+qry = ('CREATE TABLE "%s" ("%s" INTEGER PRIMARY KEY AUTOINCREMENT, "%s" INTEGER, "%s" INTEGER, "%s" INTEGER DEFAULT 0, "%s" INTEGER DEFAULT 0, FOREIGN KEY ("%s") REFERENCES "%s" ("%s"), FOREIGN KEY ("%s") REFERENCES "%s" ("%s"));' % (
+  Join.TABLE, Join.ID,
+  Join.NOUN_ID, Join.MEASURE_WORD_ID,
+  Join.CORRECT, Join.INCORRECT,
+  Join.NOUN_ID, Noun.TABLE, Noun.ID,
+  Join.MEASURE_WORD_ID, MeasureWord.TABLE, MeasureWord.ID
+));
+print qry
+cur.execute(qry)
 
-# Read data into memory
-with open('nouns.json') as f:
-  nouns = json.load(f)
-with open('measure_words.json') as f:
-  measure_words = json.load(f)
-with open('nouns_measure_words.json') as f:
-  nouns_measure_words = json.load(f)
+# Read data into db
+with open('data/nouns.json') as f:
+  for noun in json.load(f):
+    qry = ('INSERT INTO "%s" ("%s", "%s", "%s") VALUES ("%s", "%s", "%s");' % (
+      Noun.TABLE, Noun.HANZI, Noun.PINYIN, Noun.ENGLISH,
+      noun[Noun.HANZI], noun[Noun.PINYIN], noun[Noun.ENGLISH]
+    ));
+    print qry
+    cur.execute(qry)
+    con.commit()
+     
+with open('data/measure_words.json') as f:
+  for measure_word in json.load(f):
+    qry = ('INSERT INTO "%s" ("%s", "%s", "%s") VALUES ("%s", "%s", "%s");' % (
+      MeasureWord.TABLE, MeasureWord.HANZI,
+      MeasureWord.PINYIN, MeasureWord.ENGLISH,
+      measure_word[MeasureWord.HANZI], measure_word[MeasureWord.PINYIN],
+      measure_word[MeasureWord.ENGLISH]
+    ));
+    print qry
+    cur.execute(qry)
+    con.commit()
+
+with open('data/nouns_measure_words.json') as f:
+  for noun_measure_word in json.load(f):
+    # Get noun id
+    noun_hanzi = noun_measure_word['noun']
+    cur.execute('SELECT "%s" FROM "%s" WHERE %s = "%s";' % (
+      Noun.ID, Noun.TABLE, Noun.HANZI, noun_hanzi
+    ))
+    noun_id = cur.fetchone()[0]
+    # Get measure words id
+    measure_word_hanzi = noun_measure_word['measure_word']
+    cur.execute('SELECT "%s" FROM "%s" WHERE "%s" = "%s";' % (
+      MeasureWord.ID, MeasureWord.TABLE, MeasureWord.HANZI, measure_word_hanzi
+    ))
+    measure_word_id = cur.fetchone()[0]
+    # Insert
+    cur.execute('INSERT INTO "%s" ("%s", "%s") VALUES ("%s", "%s");' %(
+      Join.TABLE, Join.NOUN_ID, Join.MEASURE_WORD_ID, noun_id, measure_word_id
+    ))
+    con.commit()
+    
+con.commit()
+con.close()
